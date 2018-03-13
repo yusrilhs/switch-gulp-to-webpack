c9901cce919f30cf3cc49aafeddf6fd61fddb086
Adam Pałaszyński <35331661+appalaszynski@users.noreply.github.com>
2018-03-13 08:44:50 +0100

    Organize Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,121 +1,141 @@
-const path = require("path");
+const path = require('path');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HTMLPlugin = require('html-webpack-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   const isProduction = env === 'production';
 
   return {
-    entry: ["./src/js/App.js", "./src/scss/main.scss"],
+    entry: ['./src/scripts/App.js', './src/styles/main.scss'],
     output: {
       path: path.join(__dirname, 'public'),
-      filename: 'scripts/bundle.js'
+      filename: 'assets/scripts/bundle.js'
     },
     module: {
-      rules: [{
-        test: /\.html$/,
-        use: [{
-          loader: "html-loader",
+      rules: [
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                removeAttributeQuotes: isProduction,
+                collapseWhitespace: isProduction,
+                html5: isProduction,
+                minifyCSS: isProduction,
+                removeComments: isProduction,
+                removeEmptyAttributes: isProduction,
+                removeRedundantAttributes: isProduction,
+                useShortDoctype: isProduction,
+                removeStyleLinkTypeAttributes: isProduction,
+                keepClosingSlash: isProduction,
+                minifyJS: isProduction,
+                minifyURLs: isProduction,
+                attrs: [':data-src', ':src', ':xlink:href']
+              }
+            }
+          ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'assets/images/',
+                name: '[name].[ext]'
+              }
+            },
+            {
+              loader: 'img-loader',
+              options: {
+                enabled: isProduction
+              }
+            }
+          ]
+        },
+        {
+          test: /\.svg$/,
+          loader: 'svg-sprite-loader',
           options: {
-            minimize: isProduction,
-            removeAttributeQuotes: isProduction,
-            collapseWhitespace: isProduction,
-            html5: isProduction,
-            minifyCSS: isProduction,
-            removeComments: isProduction,
-            removeEmptyAttributes: isProduction,
-            removeRedundantAttributes: isProduction,
-            useShortDoctype: isProduction,
-            removeStyleLinkTypeAttributes: isProduction,
-            keepClosingSlash: isProduction,
-            minifyJS: isProduction,
-            minifyURLs: isProduction,
-            attrs: [':data-src', ':src', ":xlink:href"]
-          }
-        }]
-      },
-      {
-        test: /\.(png|jpe?g|gif)/i,
-        use: [{
-          loader: "file-loader",
-          options: {
-            name: "images/[name].[ext]",
+            extract: true,
+            spriteFilename: 'images/sprite.svg'
           }
         },
         {
-          loader: "img-loader"
+          test: /\.s|css$/,
+          exclude: [/node_modules/, /\.svg$/],
+          use: ExtractTextPlugin.extract({
+            publicPath: '../../',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                  modules: true,
+                  localIdentName: '[local]'
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
         }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-sprite-loader',
-        options: {
-          extract: true,
-          spriteFilename: 'images/sprite.svg'
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          publicPath: '../',
-          use: [{
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              url: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-          ]
-        })
-      },
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }
       ]
     },
+
+    // Plugins
     plugins: [
-      new HtmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "index.html"
+      new HTMLPlugin({
+        filename: 'index.html',
+        template: './src/index.html'
       }),
-      new HtmlWebPackPlugin({
-        template: "./src/about.html",
-        filename: "about.html"
-      }),
-      new ExtractTextPlugin({
-        filename: "styles/style.css",
+      new HTMLPlugin({
+        filename: 'about.html',
+        template: './src/about.html'
       }),
       new SpriteLoaderPlugin({
         plainSprite: true
       }),
+      new ExtractTextPlugin('assets/styles/styles.css'),
       new BrowserSyncPlugin({
         host: 'localhost',
         port: 3000,
         proxy: 'http://localhost:8080/',
         notify: false,
         open: false
-      }, {
+      },
+        {
           reload: false
         })
     ],
+
+    // Resolve extensions and modules paths
+    resolve: {
+      modules: ['./src', './node_modules']
+    },
+
+    // Source maps
     devtool: isProduction ? 'source-map' : 'inline-source-map',
+
+    // Webpack Dev Server configuration
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
@@ -126,5 +146,5 @@ module.exports = (env) => {
         errorDetails: true
       }
     }
-  };
+  }
 }
